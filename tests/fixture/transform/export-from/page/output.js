c07e3e558855e5612ff0f80572333bd4f4181673
orionmiz <x3ph3r@naver.com>
2022-08-20 05:19:24 +0900

    Add tests for exporting page from source

---
+++ tests/fixture/transform/export-from/page/output.js
@@ -0,0 +1,4 @@
+import { withSuperJSONPage as _withSuperJSONPage } from "next-superjson-plugin/tools";
+import { foo as _NEXT_SUPERJSON_IMPORTED_PAGE } from 'source';
+export default _withSuperJSONPage(_NEXT_SUPERJSON_IMPORTED_PAGE);
+export { getServerSideProps } from 'source';
